63af172e3d1ab4057edd38b746400530777e86ab
Rober Martín H <rmh.dev@gmail.com>
2012-12-23 21:27:23 +0100

    testing atomic weight and density values parser

--- scraping/elementParser.py
+++ scraping/elementParser.py
@@ -3,7 +3,7 @@ def parseAtomicNumber(value):
     return int(value)
 
 def parseGroup(value):
-    if (value == ""):
+    if (value == "" or value == u'\xe2\x80\x93'):
         return None
     return int(value)
 
@@ -11,6 +11,23 @@ def parsePeriod(value):
     return int(value)
 
 def parseTemperatureKelvin(value):
+    if (value == "" or value == u'\xe2\x80\x93'):
+        return None
+    elements = value.split()
+    if len(elements) > 1:
+        return float(elements[0])
+    return float(value)
+    
+def parseAtomicWeight(value):
+    extra = None
+    if value.find('[') == 0:
+        weight = value[value.find('[')+1:value.find(']')]
+    else:
+        weight = value[:value.find('(')]
+        extra = int(value[value.find('(')+1:value.find(')')])
+    return [float(weight), extra]
+
+def parseDensity(value):
     if (value == "" or value == u'\xe2\x80\x93'):
         return None
     return float(value)
--- scraping/elementParserTests.py
+++ scraping/elementParserTests.py
@@ -13,6 +13,7 @@ class ElementParserTest(unittest.TestCase):
         self.assertEqual(2, parseGroup(" 2"))
         self.assertEqual(3, parseGroup("3 "))
         self.assertEqual(None, parseGroup(""))
+        self.assertEqual(None, parseGroup(u'\xe2\x80\x93'))
 
     def testParsePeriod(self):
         self.assertEqual(1, parsePeriod("1"))
@@ -25,6 +26,20 @@ class ElementParserTest(unittest.TestCase):
         self.assertEqual(14.175, parseTemperatureKelvin("14.175"))
         self.assertEqual(None, parseTemperatureKelvin(""))
         self.assertEqual(None, parseTemperatureKelvin(u'\xe2\x80\x93'))
+        self.assertEqual(3915, parseTemperatureKelvin("3915 (Sublimates)"))
+
+    def testParseAtomicWeight(self):
+        self.assertEqual([1.008, 1], parseAtomicWeight("1.008(1)"))
+        self.assertEqual([4.002602, 2], parseAtomicWeight("4.002602(2)"))
+        self.assertEqual([44.955912, 6], parseAtomicWeight("44.955912(6)"))
+        self.assertEqual([98, None], parseAtomicWeight("[98]"))
+
+    def testParseDensity(self):
+        self.assertEqual(7, parseDensity("7"))
+        self.assertEqual(9.32, parseDensity("9.32"))
+        self.assertEqual(None, parseDensity(""))
+        self.assertEqual(None, parseDensity(u'\xe2\x80\x93'))
+
 
 
 if __name__ == "__main__":
